b97a2a833f56c2874d51dc0c51235d6e51ba95e4
Amanw-25 <amanwairagkar@gmail.com>
2024-04-11 21:11:16 +0530

    Updated

--- models/listing.js
+++ models/listing.js
@@ -1,45 +1,42 @@
-const mongoose=require("mongoose");
-const Schema=mongoose.Schema;
-const Review=require("./review.js");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review.js");
 const { string } = require("joi");
 
-const listingSchema=new Schema({
-    title:{
-        type:String,
-        required:true,
-    },
-    description:String,
-    image:{
-       url:String,
-       filename:String
-    },
-    price:Number,
-    location:String,
-    country:String,
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    url: String,
+    filename: String,
+  },
+  price: Number,
+  location: String,
+  country: String,
 
-    // We are making a review which will be taken from review.js and it will be one to many relationship in mongodb.
-    reviews:[
-        {
-            type:Schema.Types.ObjectId,
-            ref:"Review",
-        }
-    ],
-    owner:{
-        type:Schema.Types.ObjectId,
-        ref:"User",
+  // We are making a review which will be taken from review.js and it will be one to many relationship in mongodb.
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
     },
-
+  ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
-
 
 //Middleware :- So that when listing is delete so all the review related to listing should be delete from review collection also.
 
-listingSchema.post("findOneAndDelete",async(listing)=>{
-
-    if(listing){
-    await Review.deleteMany({_id: {$in: listing.reviews} });
-    }
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
 });
 
-const Listing=mongoose.model("Listing",listingSchema);
-module.exports=Listing;
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
