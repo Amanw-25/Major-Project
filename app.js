6215dbb2120d7b4eba729957a032e887bf48f95a
Amanw-25 <amanwairagkar@gmail.com>
2024-04-11 22:17:02 +0530

    Updated

--- app.js
+++ app.js
@@ -10,14 +10,11 @@ const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const path = require("path");
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const WrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema} = require("./schema.js");
-//const { reviewSchema }=require("./schema.js");
 const Review=require("./models/review.js");
 const wrapAsync = require("./utils/wrapAsync.js");
-// const review = require("./models/review.js");
 const listingsRouter= require("./routes/listing.js");
 const reviewsRouter=require("./routes/review.js");
 const userRouter=require("./routes/user.js");
@@ -50,8 +47,6 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-
-
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     crypto: {
@@ -60,16 +55,11 @@ const store = MongoStore.create({
     touchAfter: 24 * 3600,
 });
 
-
-
-
 store.on("error", (err) => {
     console.log("ERROR in MONGO SESSION STORE", err);
 });
 
 console.log('Secret from process.env:', process.env.SECRET);
-
-
 
 const sessionOptions = {
     store: store,
@@ -77,7 +67,7 @@ const sessionOptions = {
     resave: false,
     saveUninitialized: true,
     cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000, // Cookie expires after 7 days
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
     }
@@ -86,42 +76,36 @@ const sessionOptions = {
 app.use(session(sessionOptions));
 app.use(flash());
 
-
-
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.currUser = req.user; // Make sure this middleware is placed after authentication middleware
+    res.locals.currUser = req.user;
     next();
 });
-
 
 app.use("/listings",listingsRouter);
 app.use("/listings/:id/reviews",reviewsRouter);   
 app.use("/",userRouter);
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
-app.all("*", (req, res, next) => {                       // If any route other than above then show this error
+app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found !!"));
     res.redirect("/listings");
-
 });
 
 app.use((err, req, res, next) => {
-    // res.send("Something went Wrong !!!");
     let { statusCode = 500, message = "Something Went Wrong !" } = err;
-    //res.status(statusCode).send(message);
     res.render("./listings/error.ejs", { message });
-
 });
-
 
 app.listen(8080, () => {
     console.log(`Server listening at ${port}`);
